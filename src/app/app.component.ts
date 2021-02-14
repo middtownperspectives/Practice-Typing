71b1c9fd73103f4cd1c9aeaf140f5748b10b7478
middtown <aarontm9@gmail.com>
2021-02-14 10:46:15 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'practice-typing';
+
+  title = 'practice typing';
+  randomText = lorem.sentence().split('');
+  userEnteredText = '';
+
+  onInput(event: any) {
+    this.userEnteredText = event.target.value
+  }
+
+  compareLetter(randomLetter: string, userInputLetter: string) {
+
+    if(!userInputLetter) {
+      return 'pending'
+    }
+
+    return (randomLetter === userInputLetter) ? 'correct' : 'incorrect'
+
+  }
+
 }
